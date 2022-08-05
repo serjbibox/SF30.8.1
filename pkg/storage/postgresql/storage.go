84183dc179fac491b7863b78b2329e68e61eabbb
Sergey <serj_bibox@mail.ru>
2022-08-05 14:55:46 +0600

    storage constructor added

--- pkg/storage/postgresql/storage.go
+++ pkg/storage/postgresql/storage.go
@@ -1,1 +1,22 @@
 package postgresql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Storage struct {
+	db *pgxpool.Pool
+}
+
+func New(constr string) (*Storage, error) {
+	db, err := pgxpool.Connect(context.Background(), constr)
+	if err != nil {
+		return nil, err
+	}
+	s := Storage{
+		db: db,
+	}
+	return &s, nil
+}
